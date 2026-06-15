3bd06b2f411a6e7b8da350651d14a1f063b3f7c9
abner <qw0207060413@gmail.com>
2026-06-15 17:39:24 +0800

    fix: qt env

--- lua/hyprconf/env.lua
+++ lua/hyprconf/env.lua
@@ -20,7 +20,7 @@ local variables = {
   -- Toolkit scaling and appearance
   QT_AUTO_SCREEN_SCALE_FACTOR = "1",
   QT_WAYLAND_DISABLE_WINDOWDECORATION = "1",
-  QT_QPA_PLATFORMTHEME = "gtk3",
+  QT_QPA_PLATFORMTHEME = "qt6ct",
   QT_SCALE_FACTOR = "1",
   QT_QUICK_CONTROLS_STYLE = "org.hyprland.style",
   GDK_SCALE = "1",
