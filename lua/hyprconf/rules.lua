fc2804b7b4bcb75a716ae4b8577f6563458b5f25
abner <qw0207060413@gmail.com>
2026-08-10 01:09:52 +0800

    chore: edit steam_app* window rule

--- lua/hyprconf/rules.lua
+++ lua/hyprconf/rules.lua
@@ -440,12 +440,7 @@ local window_rules = {
     match = { class = "^(steam_app.*)$" },
     workspace = "10",
     no_initial_focus = true,
-  },
-  {
-    name = "stalker",
-    match = { title = "^(STALKER.*)$" },
-    workspace = "10",
-    no_initial_focus = true,
+    confine_pointer = false,
   },
 }
 
